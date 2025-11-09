7bff580331d011b00654fbc977fede9a7c17a4a1
bensonibeabuchi <ibeabuchibenson@gmail.com>
2025-11-09 01:08:40 -0800

    moved to docker registry 23

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,14 +2,7 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
 
 output "app_service_url" {
   value = azurerm_linux_web_app.app.default_hostname
-}
-
-output "key_vault_uri" {
-  value = azurerm_key_vault.kv.vault_uri
 }
